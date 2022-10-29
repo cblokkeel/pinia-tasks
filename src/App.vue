2a6d683e0fdffc7d7efe7c6f4fafa95733a77951
cblokkeel <cblokkeel@partner.auchan.fr>
2022-10-30 00:09:30 +0200

    use storeToRefs function

--- src/App.vue
+++ src/App.vue
@@ -14,32 +14,38 @@
       <button @click="handleUpdateFilter('favs')">Favs tasks</button>
     </nav>
 
-    <div class="loading" v-if="taskStore.isLoading">
+    <div class="loading" v-if="isLoading">
       Loading tasks...
     </div>
 
-    <div class='task-list' v-if="filter === 'all' && !taskStore.isLoading">
-      <p>You have {{ taskStore.getTotalCount }} tasks left to do.</p>
-      <div v-for='task in taskStore.tasks' key="task.id">
+    <div class='task-list' v-if="filter === 'all' && !isLoading">
+      <p>You have {{ getTotalCount }} tasks left to do.</p>
+      <div v-for='task in tasks' key="task.id">
         <TaskDetails :task='task' />
       </div>
     </div>
-    <div class="task-list" v-if="filter === 'favs' && !taskStore.isLoading">
-      <p>You have {{ taskStore.getFavoritesTasksCount }} favs tasks to do.</p>
-      <div v-for='task in taskStore.getFavoritesTasks' key="task.id">
+    <div class="task-list" v-if="filter === 'favs' && !isLoading">
+      <p>You have {{ getFavoritesTasksCount }} favs tasks to do.</p>
+      <div v-for='task in getFavoritesTasks' key="task.id">
         <TaskDetails :task='task' />
       </div>
     </div>
+
+    <button @click="handleResetTaskStore">reset</button>
   </main>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
 import { ref } from 'vue';
 import TaskDetails from './components/TaskDetails.vue';
 import TaskForm from './components/TaskForm.vue';
 import { useTaskStore } from './stores/TaskStore';
 
 const taskStore = useTaskStore();
+
+const { tasks, isLoading, getFavoritesTasks, getFavoritesTasksCount, getTotalCount } = storeToRefs(taskStore);
+
 taskStore.loadTasks();
 
 const filter = ref<'all' | 'favs'>('all');
@@ -47,4 +53,6 @@ const filter = ref<'all' | 'favs'>('all');
 const handleUpdateFilter = (newFilter: 'all' | 'favs') => {
   filter.value = newFilter;
 }
+
+const handleResetTaskStore = () => { taskStore.$reset(); }
 </script>
