fba8afb84709d669a5bc8599955f1d33aa225517
cblokkeel <cblokkeel@partner.auchan.fr>
2022-10-30 00:11:30 +0200

    prevent user from adding empty task

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -16,11 +16,11 @@ import { ref } from 'vue';
 const taskStore = useTaskStore();
 
 const handleSubmit = () => {
-    if (newTask.value.length > 0) {
+    if (newTask.value.trim().length > 0) {
         // not a best practice to use math.random for id here. in a real world project, i would have used uuid for ids
         taskStore.addTask({ id: Math.floor(Math.random() * 10000), title: newTask.value, isFav: false });
-        newTask.value = '';
     }
+    newTask.value = '';
 }
 
 const newTask = ref<string>('');
